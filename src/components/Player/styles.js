8f7199ab46ba4b35a3539803dfe197d8024fb8d8
ArtSantana <artsantana1457@hotmail.com>
2020-10-25 17:16:38 -0300

    Created style for fullscreen video

---
+++ src/components/Player/styles.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  fullScreen: {
+    backgroundColor: '#000',
+    height: '100%',
+    position: 'absolute',
+    width: '100%',
+  },
+});
+
+export default styles;
