5ffbf14757d5ac575b4394101443ff39c3d7167d
Arthur Santana <artsantana1457@gmail.com>
2020-12-07 12:45:48 -0300

    Added android sourceDir to rn-cli.config.js

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,7 +1,14 @@
 module.exports = {
+  project: {
+    android: {
+      sourceDir: './example/',
+    },
+  },
+
   getTransformModulePath() {
     return require.resolve('react-native-typescript-transformer');
   },
+
   getSourceExts() {
     return ['ts', 'tsx'];
   },
